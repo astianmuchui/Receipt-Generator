3bc51c8a9b6a41732d2d6101129fe9d07fbb65ce
Seb Astian <Sebastianmuchui79@gmail.com>
2021-01-03 20:07:13 +0300

    file to fetch data

--- database/fetch_database.php
+++ database/fetch_database.php
@@ -1,4 +1,9 @@
 <?php
     
-//   require(.);
+  require('connect_database.php');
+    $query = "SELECT * FROM sales";
+    $result = mysqli_query($connection,$query);
+    $products = mysqli_fetch_all($result,MYSQLI_ASSOC);
+    mysqli_free_result($result);
+    mysqli_close($connection);  
 ?>
